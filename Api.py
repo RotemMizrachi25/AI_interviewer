4fb748dd46f55da33aa2db9e742615806635ad8d
gal maimon <gal.maimon@post.runi.ac.il>
2024-06-15 13:39:09 +0300

    building more of the api

--- Api.py
+++ Api.py
@@ -3,8 +3,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/emotions', methods=['POST'])
-def calculate_percentages():
+@app.route('/affects', methods=['POST'])
+def dominante_moods():
+    dominante = []
     data = request.get_json()  # This will get the JSON data sent with the POST
     if not data:
         return jsonify({'error': 'No data received'}), 400
@@ -12,10 +13,56 @@ def calculate_percentages():
     print("Received data:", data)  # Log the received data
     print(type(data), type(data['affects'][0]))
 
-    # Example: return received data or some processed result
-    return jsonify({'message': 'Data processed', 'yourData': data}), 300
+    for key,val in data['affects'][0].items():
+        if val > 0.5:
+            dominante.append(key)
 
+    print("dominate feelings are: " + ", ".join(dominante))
+        
+    # Example: return received data or some processed result
+    return jsonify({'message': 'Data processed', 'yourData': data}), 200
+
+@app.route('/valence', methods=['POST'])
+def valence():
+    dominante = []
+    data = request.get_json()  # This will get the JSON data sent with the POST
+    if not data:
+        return jsonify({'error': 'No data received'}), 400
+
+    print("Received data:", data)  # Log the received data
+    print(type(data), type(data['affects'][0]))
+
+    for key,val in data['affects'][0].items():
+        if val > 0.5:
+            dominante.append(key)
+
+    print("dominate feelings are: " + ", ".join(dominante))
+        
+    # Example: return received data or some processed result
+    return jsonify({'message': 'Data processed', 'yourData': data}), 200
+
+@app.route('/arousel', methods=['POST'])
+def arousel():
+    data = request.get_json()  # This will get the JSON data sent with the POST
+    if not data:
+        return jsonify({'error': 'No data received'}), 400
+
+    print("Received data:", data)  # Log the received data
+
+    # Example: return received data or some processed result
+    return jsonify({'message': 'Data processed', 'yourData': data}), 200
+
+@app.route('/attention', methods=['POST'])
+def attention():
+    data = request.get_json()  # This will get the JSON data sent with the POST
+    if not data:
+        return jsonify({'error': 'No data received'}), 400
+
+    print("Received data:", data)  # Log the received data
+        
+    # Example: return received data or some processed result
+    return jsonify({'message': 'Data processed', 'yourData': data}), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
