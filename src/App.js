e56547ac392d5ca149fe96f3018a561f3ed57b84
Romi <romicohen1@gmail.com>
2024-08-04 16:45:13 +0300

    initial toolbar

--- src/App.js
+++ src/App.js
@@ -1,139 +1,31 @@
-import {useEffect, useRef, useState} from "react";
-import { useExternalScript } from "./helpers/ai-sdk/externalScriptsLoader";
-import { getAiSdkControls } from "./helpers/ai-sdk/loader";
-//import VideoComponent from "./helpers/ai-sdk/VideoAnalyzer";
-import './App.css';
+// Filename - App.js
 
-import GenderComponent from "./components/GenderComponent";
-import AgeComponent from "./components/AgeComponent";
-import DominantEmotionComponent from "./components/DominantEmotionComponent";
-import FeatureComponent from "./components/FeatureComponent";
-import EngagementComponent from "./components/EngagementComponent";
-import FaceTrackerComponent from "./components/FaceTrackerComponent";
-import MoodComponent from "./components/MoodComponent";
-import EmotionBarsComponent from "./components/EmotionBarsComponent";
+import React from "react";
+import Nav from "./nav";
+import {
+    HashRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import {Home} from "./pages/home";
+import Simulation from "./pages/simulation"
+import {Page1} from "./pages/page1";
+
 
 function App() {
+    return (
+        <>
+            <Router>
+                <Nav/>
+                <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/simulation" element={<Simulation />} />
+                        <Route path="/page1" element={<Page1 />} />
 
-  const mphToolsState = useExternalScript("https://sdk.morphcast.com/mphtools/v1.0/mphtools.js");
-  const aiSdkState = useExternalScript("https://ai-sdk.morphcast.com/v1.16/ai-sdk.js");
-  const videoEl = useRef(undefined)
-  // Mood Data
-  const [affects, setAffects] = useState([]);
-  //Engagement
-  const [valence, setValence] = useState([]);
-  const [attention, setAttention] = useState([]);
-  const [arousal, setArousal] = useState([]);
-
-  const moodApiCaller = async () => {
-    const response = await fetch(`http://localhost:5000/affects`,{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({affects: affects})
-    });
-    //console.log(response);
-    const response_json = await response.json();
-
-    console.log(response_json.results);
-  };
-
-  const engageApiCaller = async (fieldName, field) => {
-    const response = await fetch(`http://localhost:5000/${fieldName}`,{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({[fieldName]: field})
-    });
-
-    const response_json = await response.json();
-    console.log(response_json.results);
-
-  };
-
-  const handleMoodData = (newAffects) => {
-    setAffects(affects => [...affects, newAffects]);
-  };
-
-  const handleArousalValencData = (newArousal, newValence) => {
-    setArousal(arousal => [...arousal, newArousal]);
-    setValence(valence => [...valence, newValence]);
-  }
-  const handleAttentionData = (newAttention) => {
-    setAttention(attention => [...attention, newAttention]);
-  }
-
-
-
-  useEffect(() => {
-    console.log("MoodData from comp: ", affects);
-    if(affects.length === 120){
-      console.log("Type MoodData:", typeof moodData);
-      moodApiCaller();
-    }
-  }, [affects]);
-
-  useEffect(() => {
-    console.log("EngageData from comp: ", arousal, valence, attention);
-    if(arousal.length === 120){
-      engageApiCaller("arousal", arousal);
-    }
-    if(valence.length === 120){
-      engageApiCaller("valence", valence);
-    }
-    if(attention.length === 400){
-      engageApiCaller("attention", attention);
-    }
-  }, [arousal, valence, attention]);
-
-
-
-  useEffect(() => {
-    videoEl.current = document.getElementById("videoEl");
-    async function getAiSdk (){
-      if(aiSdkState === "ready" && mphToolsState === "ready"){
-        const { source, start } = await getAiSdkControls();
-
-      await source.useCamera({
-        toVideoElement: document.getElementById("videoEl"),
-      });
-        await start();
-        
-      }
-     
-    }
-    getAiSdk();
-  }, [aiSdkState, mphToolsState]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{display:"flex", flexDirection: "column", alignItems:"center"}}>
-          <div style={{width:"640px", height: "480px", position:"relative"}}>
-            <video id="videoEl"></video>
-            <FaceTrackerComponent videoEl={videoEl}></FaceTrackerComponent>
-          </div>
-          <GenderComponent></GenderComponent>
-          <hr className="solid" style={{width:"100%"}}></hr>
-          <DominantEmotionComponent></DominantEmotionComponent>
-          <hr className="solid" style={{width:"100%"}}></hr>
-          <AgeComponent></AgeComponent>
-          <hr className="solid" style={{width:"100%"}}></hr>
-          <FeatureComponent></FeatureComponent>
-          <hr className="solid" style={{width:"100%"}}></hr>
-          <EngagementComponent onDataAttention={handleAttentionData} onDataArousalValence={handleArousalValencData}/>
-          <hr className="solid" style={{width:"100%"}}></hr>
-          <MoodComponent onDataRecieve={handleMoodData}/>
-          <hr className="solid" style={{width:"100%"}}></hr>
-          <EmotionBarsComponent></EmotionBarsComponent>
-          <hr className="solid" style={{width:"100%"}}></hr>
-          
-        </div>
-      </header>
-    </div>
-  );
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/nav.js
@@ -0,0 +1,66 @@
+import {Toolbar, AppBar, Typography, Button, Tabs, Tab} from '@mui/material';
+import { cyan } from '@mui/material/colors';
+import {useEffect, useState} from "react";
+import {Link, useLocation} from "react-router-dom";
+// "use client"
+
+
+
+const Nav = () => {
+
+
+    const [value, setValue] = useState(0);
+
+    // Use location to update the tab selection based on the current path
+    const location = useLocation();
+
+    // Update the tab selection when the location changes
+    useEffect(() => {
+        if (location.pathname === "/") {
+            setValue(0);
+        } else if (location.pathname === "/simulation") {
+            setValue(1);
+        } else if (location.pathname === "/page1") {
+            setValue(2);
+        }
+    }, [location]);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return(
+        <div>
+            <AppBar sx={{background:"#063970"}}>
+                <Toolbar>
+                    <Typography>AI Interview Assistant</Typography>
+                    <Tabs
+                        sx={{marginLeft:"auto"}}
+                        textColor="inherit"
+                        value={value}
+                        onChange={(e, value) => handleChange}
+                        indicatorColor="primary">
+                        <Tab label="Home" component={Link} to="/" />
+                        <Tab label="Start" component={Link} to="/simulation" />
+                        <Tab label="Page 1" component={Link} to="/page1" />
+                    </Tabs>
+                    {/*<Button*/}
+                    {/*    href="/"*/}
+                    {/*    variant={"outlined"}*/}
+                    {/*    sx={{marginLeft:"10px"}}*/}
+                    {/*    color="primary">Home</Button>*/}
+                    {/*<Button*/}
+                    {/*    //variant={"contained"}*/}
+                    {/*    sx={{marginLeft:"10px"}}*/}
+                    {/*    color="primary"*/}
+                    {/*    href="/simulation">Start</Button>*/}
+                    {/*<Button*/}
+                    {/*    variant={"contained"}*/}
+                    {/*    sx={{marginLeft:"10px"}}*/}
+                    {/*    color="primary">hello</Button>*/}
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+};
+export default Nav;
